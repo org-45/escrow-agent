873234cea82de8ac4363cacc79216e6430c9c79c
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2024-08-26 10:16:56 +0545

    enabling max retries

--- internal/db/db.go
+++ internal/db/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -12,20 +13,29 @@ import (
 var DB *sqlx.DB
 
 func InitDB() {
-    dbHost := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
-    dbUser := os.Getenv("DB_USER")
-    dbPassword := os.Getenv("DB_PASSWORD")
-    dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
-    dataSourceName := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-        dbHost, dbPort, dbUser, dbPassword, dbName)
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
 
-    var err error
-    DB, err = sqlx.Connect("postgres", dataSourceName)
-    if err != nil {
-        log.Fatalf("Error connecting to the database: %v", err)
-    }
+	maxRetries := 5
+	retryInterval := 2 * time.Second
 
-    log.Println("Database connection established")
+	var err error
+	for i := 0; i < maxRetries; i++ {
+		DB, err = sqlx.Connect("postgres", dataSourceName)
+		if err == nil {
+			log.Println("Database connection established")
+			return
+		}
+
+		log.Printf("Error connecting to the database (attempt %d/%d): %v", i+1, maxRetries, err)
+		time.Sleep(retryInterval)
+	}
+
+	log.Fatalf("Failed to connect to the database after %d attempts: %v", maxRetries, err)
 }
