998d8be78b0db24759ff129fc8710c172f2f04fd
chauchausoup <7krishna7dhakal7@gmail.com>
2025-02-04 20:36:31 +0545

    role based transactions table

--- db_init/init-db.sql
+++ db_init/init-db.sql
@@ -1,7 +1,10 @@
 -- users
 -- transactions
+    -- enforce_buyer_seller_roles
+    -- trigger : transactions_role_check       
 -- escrow_accounts
 -- transaction_logs
+-- files
 
 CREATE TABLE IF NOT EXISTS users(
 	user_id SERIAL PRIMARY KEY,
@@ -21,6 +24,30 @@ CREATE TABLE IF NOT EXISTS transactions (
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
 
+CREATE OR REPLACE FUNCTION enforce_buyer_seller_roles()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Ensure buyer has 'buyer' role
+    IF NOT EXISTS (SELECT 1 FROM users WHERE user_id = NEW.buyer_id AND role = 'buyer') THEN
+        RAISE EXCEPTION 'Invalid buyer_id: User is not a buyer';
+    END IF;
+
+    -- Ensure seller has 'seller' role
+    IF NOT EXISTS (SELECT 1 FROM users WHERE user_id = NEW.seller_id AND role = 'seller') THEN
+        RAISE EXCEPTION 'Invalid seller_id: User is not a seller';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--attach trigger to transactions table
+CREATE TRIGGER transactions_role_check
+BEFORE INSERT OR UPDATE ON transactions
+FOR EACH ROW
+EXECUTE FUNCTION enforce_buyer_seller_roles();
+
+
 CREATE TABLE escrow_accounts (
     escrow_id SERIAL PRIMARY KEY,
     transaction_id INT REFERENCES transactions(transaction_id),
@@ -37,3 +64,11 @@ CREATE TABLE transaction_logs (
     event_details TEXT,
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
+
+CREATE TABLE files(
+	id SERIAL PRIMARY KEY,
+    transaction_id INT REFERENCES transactions(transaction_id),
+	file_name TEXT NOT NULL,
+    file_path TEXT NOT NULL, --  "transactions/{transactionID}/{filename}"
+	uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
